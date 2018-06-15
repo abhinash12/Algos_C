85a185cbee8e9f8b16d5fefd4eb36104781f1487
abhinash <abhi@macs-MacBook.local>
2018-06-15 05:43:09 -0400

    practice-1

--- pyramid/index.js
+++ pyramid/index.js
@@ -14,6 +14,41 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+
+function pyramid(n, i=0,level=''){
+  const midp = Math.floor((2 * n - 1)/2);
+  if(i==n){
+    return;
+  }
+  if(level.length===2*n - 1){
+    console.log(level);
+    return pyramid(n,i+1);
+  }
+  const add = midp -i <= level.length && midp + i >= level.length ? '#' : ' ';
+  // if (midp - i <=level.length && midp + i >=level.length)
+  // {
+  //   level+='#';
+  // }
+  // else{
+  //   level+=' ';
+  // }
+  pyramid(n, i, level+add);
+}
+// function pyramid(n) {
+//   const midp = Math.floor((2 * n - 1)/2);
+//   for(let i=0;i<n;i++){
+//     let level = '';
+//     for(let j =0;j<2*n-1;j++){
+//       if(midp - i <= j && midp + i >=j){
+//         level += '#'
+//       }
+//       else{
+//         level+=' ';
+//       }
+//     }
+//     console.log(level);
+//   }
+//
+// }
 
 module.exports = pyramid;
--- steps/index.js
+++ steps/index.js
@@ -17,6 +17,49 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n, i=0, stairs = ''){
+  if(n===i){
+    return;
+  }
+  if(stairs.length === n){
+    console.log(stairs);
+    return steps(n, i+1);
+  }
+  const add = stairs.length<=i ? '#' : ' ';
+  steps(n,i,stairs+add);
+}
+
+
+// function steps(n, i = 0, stairs = '') {
+//   if(n === i){
+//     return;
+//   }
+//   if(n === stairs.length){
+//     console.log(stairs);
+//     return steps(n, i+1, stairs = '');
+//   }
+//   if(stairs.length<=i){
+//     stairs+='#';
+//   }
+//   else{
+//     stairs+=' ';
+//   }
+//   steps(n, i, stairs);
+// }
+
+// function steps(n) {
+//   for (let i=0; i<n;i++){
+//     let stairs = '';
+//     for (let j =0;j<n;j++){
+//       if(i>=j){
+//         stairs+='#';
+//       }
+//       else{
+//         stairs+=' ';
+//       }
+//     }
+//     console.log(stairs);
+//   }
+// }
 
 module.exports = steps;
